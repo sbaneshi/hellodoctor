009f3e675eecd9ddbf54a2cf1169b96655b14528
Sepehr <s.baneshi98@gmail.com>
2020-05-06 11:25:33 +0430

    Initial commit

---
+++ src/main/java/ui/hellodoctor/HelloDoctorApplication.java
@@ -0,0 +1,13 @@
+package ui.hellodoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDoctorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDoctorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ui/hellodoctor/HelloDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package ui.hellodoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
