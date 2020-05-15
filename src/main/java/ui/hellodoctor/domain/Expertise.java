503c442a7f494aba5afcbce483767a7485d23ce2
Sepehr <s.baneshi98@gmail.com>
2020-05-15 10:44:21 +0430

    MySQL fixed.

--- src/main/java/ui/hellodoctor/domain/Expertise.java
+++ src/main/java/ui/hellodoctor/domain/Expertise.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
@@ -17,7 +18,7 @@ import javax.persistence.Id;
 public class Expertise {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
--- src/main/java/ui/hellodoctor/domain/User.java
+++ src/main/java/ui/hellodoctor/domain/User.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
@@ -15,7 +16,7 @@ import javax.persistence.MappedSuperclass;
 public abstract class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String userName;
