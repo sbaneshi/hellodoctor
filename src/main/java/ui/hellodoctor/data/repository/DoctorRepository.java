8aa30c0f664ccff544b38c581a7a04a29032c058
Sepehr <s.baneshi98@gmail.com>
2020-06-05 22:49:49 +0430

    repos updated.

--- src/main/java/ui/hellodoctor/data/repository/DoctorRepository.java
+++ src/main/java/ui/hellodoctor/data/repository/DoctorRepository.java
@@ -3,6 +3,9 @@ package ui.hellodoctor.data.repository;
 import ui.hellodoctor.data.domain.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 
+    Optional<Doctor> findByUsername(String username);
 }
--- src/main/java/ui/hellodoctor/data/repository/PatientRepository.java
+++ src/main/java/ui/hellodoctor/data/repository/PatientRepository.java
@@ -1,8 +1,11 @@
 package ui.hellodoctor.data.repository;
 
-import ui.hellodoctor.data.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import ui.hellodoctor.data.domain.Patient;
+
+import java.util.Optional;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
+    Optional<Patient> findByUsername(String username);
 }
