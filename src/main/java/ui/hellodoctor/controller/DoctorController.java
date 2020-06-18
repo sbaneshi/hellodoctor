a02c42a66d9c18846820d0f5ba7ac71a29ffcce4
Sepehr <s.baneshi98@gmail.com>
2020-06-18 10:54:33 +0430

    work time requests added.

--- src/main/java/ui/hellodoctor/controller/DoctorController.java
+++ src/main/java/ui/hellodoctor/controller/DoctorController.java
@@ -1,10 +1,14 @@
 package ui.hellodoctor.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ui.hellodoctor.data.domain.Doctor;
+import ui.hellodoctor.data.domain.WorkTime;
 import ui.hellodoctor.security.SecurityUtils;
 import ui.hellodoctor.service.DoctorService;
+
+import java.time.DayOfWeek;
 
 @CrossOrigin
 @RestController
@@ -29,5 +33,16 @@ public class DoctorController extends BaseController {
         return doctorService.signUp(phoneNumber, password, expertise, city, maCode);
     }
 
+    @PostMapping("/api/doctor/worktime")
+    public WorkTime addWorkTime(@RequestParam int start24, @RequestParam int end24, @RequestParam DayOfWeek dayOfWeek) {
+        assertRole(SecurityUtils.ROLE_DOCTOR);
+        return doctorService.addWorkTime(getPhoneNumber(), start24, end24, dayOfWeek);
+    }
 
+    @DeleteMapping("/api/doctor/worktime")
+    public ResponseEntity<?> deleteWorkTime(@RequestParam int id) {
+        assertRole(SecurityUtils.ROLE_DOCTOR);
+        doctorService.deleteWorkTime(getPhoneNumber(), id);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/ui/hellodoctor/service/DoctorService.java
+++ src/main/java/ui/hellodoctor/service/DoctorService.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 import ui.hellodoctor.data.domain.Doctor;
 import ui.hellodoctor.data.domain.User;
+import ui.hellodoctor.data.domain.WorkTime;
 import ui.hellodoctor.data.repository.DoctorRepository;
+import ui.hellodoctor.data.repository.WorkTimeRepository;
 
+import java.time.DayOfWeek;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,6 +18,7 @@ import java.util.stream.Collectors;
 public class DoctorService {
 
     private final DoctorRepository doctorRepository;
+    private final WorkTimeRepository workTimeRepository;
 
     public Doctor getFullDoctor(String phoneNumber) {
         Doctor doctor = doctorRepository.findByPhoneNumber(phoneNumber).orElseThrow(IllegalArgumentException::new);
@@ -47,5 +52,31 @@ public class DoctorService {
         doctor.setPassword(password);
 
         return doctorRepository.save(doctor);
+    }
+
+    public WorkTime addWorkTime(String doctorPhoneNumber, int start24, int end24, DayOfWeek dayOfWeek) {
+        WorkTime workTime = WorkTime.builder()
+                .startHour24(start24)
+                .endHour24(end24)
+                .dayOfWeek(dayOfWeek)
+                .build();
+        workTime = workTimeRepository.save(workTime);
+
+        Doctor doctor = doctorRepository.findByPhoneNumber(doctorPhoneNumber).orElseThrow(IllegalArgumentException::new);
+        List<WorkTime> workTimes = doctor.getWorkTimes();
+        workTimes.add(workTime);
+        doctor.setWorkTimes(workTimes);
+        doctorRepository.save(doctor);
+
+        return workTime;
+    }
+
+    public void deleteWorkTime(String doctorPhoneNumber, int workTimeId) {
+        Doctor doctor  = doctorRepository.findByPhoneNumber(doctorPhoneNumber).orElseThrow(IllegalArgumentException::new);
+        List<WorkTime> workTimes = doctor.getWorkTimes();
+        workTimes = workTimes.stream().filter(workTime -> workTime.getId() != workTimeId).collect(Collectors.toList());
+        doctor.setWorkTimes(workTimes);
+
+        workTimeRepository.deleteById(workTimeId);
     }
 }
