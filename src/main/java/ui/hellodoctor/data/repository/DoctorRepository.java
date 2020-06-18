2e5124f24830d8e5c9dcdf133c342f254da03012
Sepehr <s.baneshi98@gmail.com>
2020-06-18 09:36:17 +0430

    DoctorRepository fixed.

--- src/main/java/ui/hellodoctor/data/repository/DoctorRepository.java
+++ src/main/java/ui/hellodoctor/data/repository/DoctorRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface DoctorRepository extends JpaRepository<Doctor, Object> {
+public interface DoctorRepository extends JpaRepository<Doctor, Integer> {
 
     Optional<Doctor> findByPhoneNumber(String phoneNumber);
 }
